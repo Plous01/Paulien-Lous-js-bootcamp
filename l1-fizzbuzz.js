d5b7be1d376585dce43b0c85d6a8926aa17fcf7a
Paulien <32821277+Plous01@users.noreply.github.com>
2019-02-14 10:30:08 +0100

    Homework lesson 1 update
    Beautify

--- l1-fizzbuzz.js
+++ l1-fizzbuzz.js
@@ -1,30 +1,24 @@
-
 //Eerste deel van de opdracht
-for (let i=1; i<100; i++){
-    if (i % 3 == 0){
-        console.log("Fizz");
-    }
-    else if (i % 5 == 0){
-        console.log("Buzz");
-    }
-    else{
-        console.log(i);
-    }
+for (let i = 1; i < 100; i++) {
+  if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
 
 
 //Tweede deel van de opdracht
-for (i=1; i<100; i++){
-    if (i % 3 == 0 && i % 5 == 0){
-        console.log("Fizzbuzz");
-    }
-    else if (i % 3 == 0){
-        console.log("Fizz");
-    }
-    else if (i % 5 == 0){
-        console.log("Buzz");
-    }
-    else {
-        console.log(i);
-    }
+for (i = 1; i < 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Fizzbuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
