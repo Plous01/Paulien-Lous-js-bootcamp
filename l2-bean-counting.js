a2d4ccbf4e6c5d73f1dba03abdc7f1c554f4415f
Paulien <32821277+Plous01@users.noreply.github.com>
2019-02-14 10:35:38 +0100

    Homework lesson 2 update
    Beautify

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,12 +1,12 @@
-//
+//Met een specifieke letter
 function countBs(str) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === "B") {
-            count = count + 1;
-        }
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "B") {
+      count = count + 1;
     }
-    return count;
+  }
+  return count;
 }
 
 console.log(countBs("Bob"));
@@ -14,15 +14,15 @@ console.log(countBs("BoB"));
 
 
 
-//
-function countChar(str, ch){
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === ch) {
-            count = count + 1;
-        }
+//Met een willekeurige letter
+function countChar(str, ch) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ch) {
+      count = count + 1;
     }
-    return count;
+  }
+  return count;
 }
 
 console.log(countChar("Peter", "e"));
