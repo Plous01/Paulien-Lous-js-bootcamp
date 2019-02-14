c6d8784629509a7b767caecda3ab15f72265587d
Paulien <32821277+Plous01@users.noreply.github.com>
2019-02-14 10:41:41 +0100

    Homework lesson 3

--- l3-flattening.js
+++ l3-flattening.js
@@ -1,1 +1,11 @@
- 
+let arrays = [
+  [1, 2, 3],
+  [4, 5],
+  [6]
+];
+
+let flatten = arrays.reduce((acc, current) => {
+  console.log(acc, current);
+  return acc.concat(current);
+}, []);
+console.log(flatten);
--- l3-vector-type.js
+++ l3-vector-type.js
@@ -1,1 +1,26 @@
- 
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vector) {
+    let sumx = this.x + vector.x;
+    let sumy = this.y + vector.y;
+    return new Vec(sumx, sumy);
+  }
+
+  minus(vector) {
+    let diffx = this.x - vector.x;
+    let diffy = this.y - vector.y;
+    return new Vec(diffx, diffy);
+  }
+
+  get length() {
+    return Math.sqrt((this.x * this.x) + (this.y * this.y));
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(3, 4).length);
