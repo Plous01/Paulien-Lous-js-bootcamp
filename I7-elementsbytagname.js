4a4113bc9af62f5692980509a2c09b4b6aa68fc1
Paulien <32821277+Plous01@users.noreply.github.com>
2019-03-05 18:07:27 +0100

    Homework frontend week 3 Beautify

--- I7-elementsbytagname.js
+++ I7-elementsbytagname.js
@@ -1,29 +1,32 @@
-<h1>Heading with a <span>span</span> element.</h1>
-<p>A paragraph with <span>one</span>, <span>two</span>
-  spans.</p>
+< h1 > Heading with a < span > span < /span> element.</h1 >
+    <
+    p > A paragraph with < span > one < /span>, <span>two</span >
+    spans. < /p>
 
-<script>
-  function byTagName(node, tagName) {
-    let result = [];
-    if (node.nodeType == Node.ELEMENT_NODE){
-      if (node.nodeName == tagName.toUpperCase()) {
-        result.push(node);
-      }
-      for (let i = 0; i < node.childNodes.length; i++) {
-        let childNodes = byTagName(node.childNodes[i], tagName);
-        for(let childNode of childNodes) {
-        	result.push(childNode);
+    <
+    script >
+    function byTagName(node, tagName) {
+        let result = [];
+        if (node.nodeType == Node.ELEMENT_NODE) {
+            if (node.nodeName == tagName.toUpperCase()) {
+                result.push(node);
+            }
+            for (let i = 0; i < node.childNodes.length; i++) {
+                let childNodes = byTagName(node.childNodes[i], tagName);
+                for (let childNode of childNodes) {
+                    result.push(childNode);
+                }
+            }
         }
-      }
+        return result;
     }
-    return result;
-  }
-  
-  console.log(byTagName(document.body, "h1").length);
-  // → 1
-  console.log(byTagName(document.body, "span").length);
-  // → 3
-  let para = document.querySelector("p");
-  console.log(byTagName(para, "span").length);
-  // → 2
-</script>
+
+console.log(byTagName(document.body, "h1").length);
+// → 1
+console.log(byTagName(document.body, "span").length);
+// → 3
+let para = document.querySelector("p");
+console.log(byTagName(para, "span").length);
+// → 2
+<
+/script>
